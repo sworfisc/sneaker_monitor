362990d70b720c15151c3f2aa810bca11897a30c
sworfisc <javier99re@gmail.com>
2022-07-26 20:00:41 +0200

    Scroll to the bottom and then retrieve all the dunks

--- main.py
+++ main.py
@@ -1,24 +1,48 @@
 import logging
-
+import time
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
+dunks_ayer = []
 
 def lista_dunks(url, driver):
+
     try: # Aceptar cookies
         driver.implicitly_wait(10)
         driver.find_element(By.XPATH, '//*[@id="gen-nav-commerce-header-v2"]/div[1]/div/div[2]/div/div[2]/div[2]/button').click()
 
     except NoSuchElementException:
-        logging.info("No hubo cookies que aceptar")
+        print("No hubo cookies que aceptar")
 
     finally:
-        all_dunks = driver.find_elements(By.CLASS_NAME, 'product-card__img-link-overlay')
-        for dunk in all_dunks:
-            print(dunk.get_attribute('href'))
+        scroll_bottom()
+        dunks = driver.find_elements(By.CLASS_NAME, 'product-card__img-link-overlay')
+        dunks_hoy = [dunk.get_attribute('href') for dunk in dunks]
+        dunks_nuevas = list(filter(lambda dunk: dunk not in dunks_ayer, dunks_hoy))
+        print(dunks_nuevas)
+
+
+def scroll_bottom():
+    SCROLL_PAUSE_TIME = 0.5
+
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 
 
 if __name__ == "__main__":
