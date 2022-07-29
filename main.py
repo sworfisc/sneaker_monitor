37791e12f3cd2d3aba3cb3b6deb65e59e37261e5
sworfisc <javier99re@gmail.com>
2022-07-29 15:38:56 +0200

    Check stock size by size

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import logging
+import multiprocessing
+import random
 import time
-
 import discord as discord
 from discord.ext import commands, tasks
-import schedule
 from selenium import webdriver
-from selenium.common import NoSuchElementException
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support import expected_conditions as EC
+from multiprocessing import Process
 
-import os
+import bot_secrets
 
 dunks_ayer = []
 dunk_nuevas = []
@@ -21,7 +20,7 @@ dunk_nuevas = []
 nike_page='https://www.nike.com/es/w?q=dunk&vst=dunk'
 options = webdriver.ChromeOptions()
 options.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
-token = os.getenv('TOKEN')
+token = bot_secrets.bot_token
 bot = discord.Client()
 
 
@@ -44,10 +43,46 @@ async def lista_dunks():
         channel = bot.get_channel(1002216168163115172)
         for link in dunks_nuevas:
             await channel.send(link)
-            time.sleep(1)
+        for link in dunks_hoy:          #TODO CREAR UNA POOL DE PROCESOS Y QUE CADA PROCESO COMPRUEBE EL STOCK DE LOS PARES QUE PUEDA
+            await comprueba_stock(driver,channel,link)
     except Exception as e:
-        logging.info("Excepción con código:", e)
+        logging.warning(f"Excepción con código: {e}")
 
+
+async def comprueba_stock(driver, channel, link):
+    #driver.get(link)
+    driver.get("https://www.nike.com/es/launch/t/dunk-low-golden-moss")
+    tallas = driver.find_elements(By.NAME, "skuAndSize")
+    disponibles = []
+    if tallas:      # Dos tipos de listings, con nombre skuAndSize y con propiedad size-available
+        for talla in tallas:
+            if not talla.get_attribute("disabled"):
+                atributo = talla.get_attribute("id")
+                disponibles.append(driver.find_element(By.XPATH,f"//*[@for='{atributo}']").text)
+
+    else:
+        tallas = driver.find_elements(By.XPATH, "//*[@data-qa='size-available']")
+        for talla in tallas:
+            disponibles.append(talla.text)
+
+    if disponibles:
+        await channel.send(link + "\n" + "\n".join(disponibles))
+    else:
+        await channel.send(link + "\nSin stock")
+
+
+
+async def work(item, count):
+    name = multiprocessing.current_process().name
+    logging.info(f'{name} started: {item}')
+    for x in range(count):
+        logging.info(f'{name}: {item} = {x}')
+        time.sleep(1)
+    logging.info(f'{name} finished')
+    return item + ' is finished'
+
+async def proc_result(result):
+    logging.info(f'result = {result}')
 
 
 @bot.event
